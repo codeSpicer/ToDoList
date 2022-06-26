69f8a8fc18d4514c95fcc3f51df55e98fe3215f4
PAVILION\aksha <akshat.lko.2001@gmail.com>
2022-06-26 15:04:10 +0530

    routing to custom made lists using express

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // NPM MODULES ----------------------------------------------------------->
 
 let express = require("express");
-let app = express();
 let ejs = require("ejs"); // for injected java script
 let mongoose = require("mongoose");
+const _ = require("lodash");
 
 // USING MODULES---------------------------------------------------------->
 
+let app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true })); // for body parsing    // USE not set USE!
 app.use(express.static("public")); // to make a public folder with all the files accesssible when deploy on cloud
@@ -19,65 +20,80 @@ mongoose.connect(
 const itemsSchema = { name: String };
 const Item = mongoose.model("Item", itemsSchema);
 
+const listSchema = { name: String, items: [itemsSchema] };
+const List = mongoose.model(" List", listSchema);
+
 // LISTENING TO SERVER----------------------------------------------------------------->
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started at port 3000.");
 });
 
-// TASK ITEMS INSERTION IN MONGO ------------------------------------------------------------->
-
-// const item1 = new Item({
-//   name: "task1",
-// });
-// const item2 = new Item({
-//   name: "task2",
-// });
-// const defaultItems = [item1, item2];
-
-// Item.insertMany(defaultItems, (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("sucess");
-//   }
-// });
-
-// GET AND POST REQUESTS---------------------------------------------------------------->
+// GET REQUESTS---------------------------------------------------------------->
 
 app.get("/", (req, res) => {
-  let today = new Date();
-
-  var day = today.toLocaleDateString("en-US", {
-    weekday: "long",
-    day: "numeric", // can be passed as a js object
-    month: "long",
-  });
-
   Item.find({}, (err, foundItems) => {
     if (err) {
       console.log(err);
     } else {
       res.render("list", {
-        listTitle: day,
+        listTitle: "Today",
         newListItem: foundItems,
-        listType: "/",
-      }); // needs to have view dir
+      });
     }
   });
 });
 
+app.get("/:customListName", (req, res) => {
+  const listName = req.params.customListName;
+  List.findOne(
+    {
+      name: listName,
+    },
+    function (err, foundList) {
+      if (!err) {
+        if (foundList) {
+          // show a that list
+          res.render("list", {
+            listTitle: foundList.name,
+            newListItem: foundList.items,
+          });
+        } else {
+          // make a new list indb
+          const list = new List({ name: listName, newListItem: foundList });
+          list.save();
+          res.redirect("/" + listName);
+        }
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+// POST REQUESTS------------------------------------------------------------------>
+
 app.post("/", (req, res) => {
+  const listName = req.body.list;
+  const itemName = req.body.newItem;
+
   if (req.body.newItem == "") {
     return;
   }
-  const item = new Item({
-    name: req.body.newItem,
-  });
+  const item = new Item({ name: itemName });
 
-  item.save();
-
-  res.redirect("/");
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, (err, foundList) => {
+      if (!err) {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      }
+    });
+  }
 });
 
 app.post("/delete", (req, res) => {
@@ -89,19 +105,3 @@ app.post("/delete", (req, res) => {
   });
   res.redirect("/");
 });
-
-// app.get("/work", (req, res) => {
-//   res.render("list", {
-//     listTitle: "Work List",
-//     newListItem: workItems,
-//     listType: "/work",
-//   });
-// });
-
-// app.post("/work", (req, res) => {
-//   if (req.body.newItem == "") {
-//     return;
-//   }
-//   workItems.push(req.body.newItem);
-//   res.redirect("/work");
-// });
